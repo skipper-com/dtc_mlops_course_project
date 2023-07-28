0b9d3439bf146316c385e80340cd8d530facf048
Alex Pilugin <skipper-com@mail.ru>
2023-07-28 15:03:41 +0000

    06 pre

--- tests/model_test.py
+++ tests/model_test.py
@@ -1,28 +1,54 @@
-from pathlib import Path
+import mlflow
+import boto3
+import os
 
-import model
+run_id = os.getenv('RUN_ID')
+print(run_id)
+
+def prepare_features(listing):
+    features = {}
+    features['neighbourhood'] = listing['neighbourhood']
+    features['room_type'] = listing['room_type']
+    features['availability_365'] = listing['availability_365']
+    features['price'] = listing['price'] if listing['price'] < 500 else 0
+    return features
+
+def get_model_location(run_id):
+    model_location = os.getenv('MODEL_LOCATION')
+
+    if model_location is not None:
+        return model_location
+
+    model_bucket = os.getenv('MODEL_BUCKET', 'mlops-course-project/mlflow')
+    experiment_id = os.getenv('MLFLOW_EXPERIMENT_ID', '2')
+
+    model_location = f's3://{model_bucket}/{experiment_id}/{run_id}/artifacts/models_mlflow'
+    return model_location
+
+
+def load_model(run_id):
+    model_path = get_model_location(run_id)
+    model = mlflow.pyfunc.load_model(model_path)
+    return model
+
 
 
 def test_prepare_features():
-    model_service = model.ModelService(None)
-
     listing = {
         "neighbourhood": "Test_neighbourhood",
         "room_type": "Test_room_type",
         "availability_365": 3.66,
         "price": 300
     }
-
-    actual_features = model_service.prepare_features(listing)
-
+    actual_features = prepare_features(listing)
     expected_fetures = {
         "neighbourhood": "Test_neighbourhood",
         "room_type": "Test_room_type",
         "availability_365": 3.66,
         "price": 300
     }
-
     assert actual_features == expected_fetures
+
 
     listing = {
         "neighbourhood": "Test_neighbourhood",
@@ -30,32 +56,23 @@ def test_prepare_features():
         "availability_365": 3.66,
         "price": 600
     }
-
-    actual_features = model_service.prepare_features(listing)
-
+    actual_features = prepare_features(listing)
     expected_fetures = {
         "neighbourhood": "Test_neighbourhood",
         "room_type": "Test_room_type",
         "availability_365": 3.66,
         "price": 0
     }
-
     assert actual_features == expected_fetures
 
 
-
-class ModelMock:
-    def __init__(self, value):
-        self.value = value
-
-    def predict(self, X):
-        n = len(X)
-        return [self.value] * n
-
-
 def test_predict():
+    model = load_model(run_id)
+
+    
+    
     model_mock = ModelMock(10.0)
-    model_service = model.ModelService(model_mock)
+    model_service = model.predict(model_mock)
 
     features = {
         "PU_DO": "130_205",
