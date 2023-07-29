551cc3b21a4261f0bc83108c84ac84d5b76c5543
Alex Pilugin <skipper-com@mail.ru>
2023-07-29 16:45:50 +0000

    06 tests

--- tests/model_test.py
+++ tests/model_test.py
@@ -1,36 +1,40 @@
-import mlflow
 import boto3
 import os
+import pickle
+import xgboost
 
-run_id = os.getenv('RUN_ID')
-print(run_id)
 
 def prepare_features(listing):
     features = {}
-    features['neighbourhood'] = listing['neighbourhood']
-    features['room_type'] = listing['room_type']
-    features['availability_365'] = listing['availability_365']
-    features['price'] = listing['price'] if listing['price'] < 500 else 0
+    features["neighbourhood"] = listing["neighbourhood"]
+    features["room_type"] = listing["room_type"]
+    features["availability_365"] = listing["availability_365"]
+    features["price"] = listing["price"] if listing["price"] < 500 else 0
     return features
 
-def get_model_location(run_id):
-    model_location = os.getenv('MODEL_LOCATION')
 
-    if model_location is not None:
-        return model_location
+def load_model():
+    model_bucket = os.getenv("MODEL_BUCKET", "mlops-course-project")
+    experiment_id = os.getenv("MLFLOW_EXPERIMENT_ID", "2")
+    run_id = os.getenv("RUN_ID", "b0f1202cf1da42439539dbf947081ac3")
 
-    model_bucket = os.getenv('MODEL_BUCKET', 'mlops-course-project/mlflow')
-    experiment_id = os.getenv('MLFLOW_EXPERIMENT_ID', '2')
+    file = f"mlflow/{experiment_id}/{run_id}/artifacts/models_pickle/lin_reg.bin"
 
-    model_location = f's3://{model_bucket}/{experiment_id}/{run_id}/artifacts/models_mlflow'
-    return model_location
+    cred = boto3.Session().get_credentials()
+    ACCESS_KEY = cred.access_key
+    SECRET_KEY = cred.secret_key
 
+    s3client = boto3.client(
+        "s3",
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+    )
 
-def load_model(run_id):
-    model_path = get_model_location(run_id)
-    model = mlflow.pyfunc.load_model(model_path)
-    return model
+    response = s3client.get_object(Bucket=model_bucket, Key=file)
 
+    body = response["Body"].read()
+    dv, model = pickle.loads(body)
+    return (dv, model)
 
 
 def test_prepare_features():
@@ -38,48 +42,46 @@ def test_prepare_features():
         "neighbourhood": "Test_neighbourhood",
         "room_type": "Test_room_type",
         "availability_365": 3.66,
-        "price": 300
+        "price": 300,
     }
     actual_features = prepare_features(listing)
     expected_fetures = {
         "neighbourhood": "Test_neighbourhood",
         "room_type": "Test_room_type",
         "availability_365": 3.66,
-        "price": 300
+        "price": 300,
     }
     assert actual_features == expected_fetures
-
 
     listing = {
         "neighbourhood": "Test_neighbourhood",
         "room_type": "Test_room_type",
         "availability_365": 3.66,
-        "price": 600
+        "price": 600,
     }
     actual_features = prepare_features(listing)
     expected_fetures = {
         "neighbourhood": "Test_neighbourhood",
         "room_type": "Test_room_type",
         "availability_365": 3.66,
-        "price": 0
+        "price": 0,
     }
     assert actual_features == expected_fetures
 
 
 def test_predict():
-    model = load_model(run_id)
-
-    
-    
-    model_mock = ModelMock(10.0)
-    model_service = model.predict(model_mock)
+    dv, model = load_model()
 
     features = {
-        "PU_DO": "130_205",
-        "trip_distance": 3.66,
+        "neighbourhood": "Midtown",
+        "room_type": "Private room",
+        "availability_365": 100,
     }
 
-    actual_prediction = model_service.predict(features)
-    expected_prediction = 10.0
+    X = dv.transform(features)
+    data = xgboost.DMatrix(X)
+
+    actual_prediction = model.predict(data)
+    expected_prediction = 225.67732
 
     assert actual_prediction == expected_prediction
